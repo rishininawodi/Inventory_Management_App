a847a83d89f721f16941102a9658bf712682bd40
Rishini <nawodirishini88@gmail.com>
2024-05-25 22:48:20 +0530

    modify

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("bodyparser");
+const cors = require("cors");
+
+const app = express()
+
+const PORT =process.env.PORT || 3000;
+
+//this part for to connct mongo DB and  start server
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(PORT,() =>{
+            console.log('sever Running on porrt ${PORT}')
+
+        })    
+
+    })
+    .catch((err) => console.log(err))
