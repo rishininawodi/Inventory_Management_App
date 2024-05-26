a7e62fab4cfda54fd279c7d99f1e1886ed90ff76
Rishini <nawodirishini88@gmail.com>
2024-05-26 11:09:50 +0530

    Modified Server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyparser = require("bodyparser");
+const bodyparser = require("body-parser");
 const cors = require("cors");
 
 const app = express()
@@ -14,9 +14,10 @@ mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(PORT,() =>{
-            console.log('sever Running on porrt ${PORT}')
+            console.log(`sever Running on porrt ${PORT}
+            `);
 
-        })    
+        });    
 
     })
-    .catch((err) => console.log(err))
+    .catch((err) => console.log(err));
