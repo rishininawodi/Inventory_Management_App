ca9e884d43f01006ccf5b7146faafd6890693940
Rishini <nawodirishini88@gmail.com>
2024-06-09 22:10:27 +0530

    Create login controller function

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -2,6 +2,7 @@ const asyncHandler = require("express-async-handler")
 const User = require("../models/userModel");
 //const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 //generate web token function
 const generateToken = (id) => {
@@ -80,7 +81,54 @@ const registerUser  = asyncHandler( async (req, res) => {
 
 //Logging User
 const logingUser =asyncHandler(async(req, res)=> {
-    res.send("Login User");
+    const {email,password} = req.body;
+
+    //validation 
+    if(!email || !password) {
+        res.status(400);
+        throw new Error("please add email and password");
+
+    }
+
+    //check if user exist
+    const user =await User.findOne({email});
+    if(!user) {
+        res.status(400);
+        throw new Error("User not found,please signup");
+
+    }
+    //User exists, check if password is correct
+    const passwordIsCorrect = await bcrypt.compare(password,user.password)
+    //generate token fot front end
+    const token= generateToken(user._id);
+    //send HTTP -only cookie for client
+    res.cookie("token", token, {
+        path: "/",  //path were cookie wiil be stored
+        httpOnly: true, //boolean parameter flags the cookie to be only used by the web server
+        expires: new Date(Date.now() +1000 * 86400), //1 day equation
+        
+        //folowing two execute only deploye.
+        sameSite: "none",
+        secure:true  //this  marks the cookie to be used only with https
+    });
+
+
+
+
+    //user information
+    if(user && passwordIsCorrect){
+        const {_id,name,email,photo,phone,bio}= user;
+        res.status(208).json({
+            _id,name,email,photo,phone,bio,token,
+           
+        });
+
+    }
+    else{
+        res.status(409);
+        throw new Error("Invalid email or password"); 
+    }
+
 });
 
 //this .js file has several controller functions.So exports module as an objects tha will have many  properties
